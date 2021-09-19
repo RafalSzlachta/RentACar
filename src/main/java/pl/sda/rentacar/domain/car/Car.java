5787a109eb111ed5b46843c047f1e8e5fcb9e796
Alan <grabowskialan@gmail.com>
2021-09-19 13:14:12 +0200

    RAC-30 Car entity fix

--- src/main/java/pl/sda/rentacar/domain/car/Car.java
+++ src/main/java/pl/sda/rentacar/domain/car/Car.java
@@ -1,6 +1,5 @@
 package pl.sda.rentacar.domain.car;
 
-import jdk.jshell.Snippet;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -25,7 +24,7 @@ public class Car {
     private String make;
     private String model;
     private Integer productionYear;
-    private RentStatus status;
+    private CarStatus status;
     private BigDecimal pricePerDay;
     private BodyType bodyType;
 }
