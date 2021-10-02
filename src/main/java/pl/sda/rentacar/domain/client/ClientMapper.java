9ecf0b224b7999af017604bde62bb0b4100895ef
RafalSzlachta <61345288+RafalSzlachta@users.noreply.github.com>
2021-10-02 12:36:19 +0200

    RAC-16 Client CRUD created (#15)
    * RAC-16 Client CRUD created
    
    * RAC-16 integration test fixes
    
    Co-authored-by: bkonczyk <bartkon@gmail.com>

---
+++ src/main/java/pl/sda/rentacar/domain/client/ClientMapper.java
@@ -0,0 +1,17 @@
+package pl.sda.rentacar.domain.client;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import static org.mapstruct.factory.Mappers.getMapper;
+
+@Mapper
+interface ClientMapper {
+
+    ClientMapper MAPPER = getMapper(ClientMapper.class);
+
+    @Mapping(target = "id", ignore = true)
+    Client mapToClient(ClientCreateRequest request);
+
+    ClientView mapToClientView(Client client);
+}
---
+++ src/test/groovy/pl/sda/rentacar/domain/client/ClientServiceIntegrationSpec.groovy
@@ -0,0 +1,136 @@
+package pl.sda.rentacar.domain.client
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ContextConfiguration
+import spock.lang.Shared
+import spock.lang.Specification
+
+@ContextConfiguration
+@SpringBootTest
+class ClientServiceIntegrationSpec extends Specification {
+
+    @Autowired
+    private ClientRepository repository
+
+    @Autowired
+    private ClientService service
+
+    def cleanup() {
+        repository.deleteAll()
+    }
+
+    @Shared
+    def client = new ClientCreateRequest(
+            "Beata",
+            "Kozidrak",
+            "beata@kozidrak.pl",
+            "222555333")
+
+    def 'should add client'() {
+        given:
+        service.addClient(client)
+
+        when:
+        def result = repository.findAll()
+
+        then:
+        with(result.first()) {
+            id != null
+            firstName == client.firstName
+            lastName == client.lastName
+            email == client.email
+            phoneNumber == client.phoneNumber
+        }
+    }
+
+    def 'should return client by given id if exist'() {
+        given:
+        service.addClient(client)
+        def all = repository.findAll()
+        def id = all.first().id
+
+        when:
+        def result = service.getClientById(id)
+
+        then:
+        with(result) {
+            firstName == client.firstName
+            lastName == client.lastName
+            email == client.email
+            phoneNumber == client.phoneNumber
+        }
+    }
+
+    def 'ShouldAddTwoClientsAndFindTwoClients'() {
+        given:
+        def secondClient = new ClientCreateRequest(
+                "Kamil",
+                "Durczok",
+                "kamil@durczok.pl",
+                "111444222")
+
+        when:
+        service.addClient(client)
+        service.addClient(secondClient)
+
+        and:
+        def clients = service.getALlClients()
+
+        then:
+        clients != null
+        clients.size() == 2
+        with(clients.last()) {
+            firstName == secondClient.firstName
+            lastName == secondClient.lastName
+            email == secondClient.email
+            phoneNumber == secondClient.phoneNumber
+        }
+    }
+
+    def 'should throw ClientNotFoundException'() {
+        when:
+        service.getClientById(10L)
+
+        then:
+        def e = thrown(ClientNotFoundException)
+        e.message == "client with id: 10 doesn't exsists"
+    }
+
+    def 'should update client with given id'() {
+        given:
+        def requestUpdate = new ClientCreateRequest(
+                "Edyta",
+                "Górniak",
+                "edyta@górniak.pl",
+                "666999666")
+
+        and:
+        service.addClient(client)
+        def id = repository.findAll().first().id
+
+        when:
+        service.updateClient(id, requestUpdate)
+        def updatedClient = service.getClientById(id)
+
+        then:
+        with(updatedClient) {
+            firstName == requestUpdate.firstName
+            lastName == requestUpdate.lastName
+            email == requestUpdate.email
+            phoneNumber == requestUpdate.phoneNumber
+        }
+    }
+
+    def 'should remove client with given id'() {
+        given:
+        service.addClient(client)
+        def id = repository.findAll().first().id
+
+        when:
+        service.removeClient(id)
+
+        then:
+        repository.findAll().isEmpty()
+    }
+}
